926ec186fbf15ed1ae379d78ed8a8cf34a6441cb
SaiSirishaR <38254544+SaiSirishaR@users.noreply.github.com>
2023-09-09 14:36:07 +0200

    Create LeetCode206-ReverseLinkedList.py

---
+++ LeetCode206-ReverseLinkedList.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+          
+###Linkedlist = group of nodes
+###node = data, pointer (pointing towards the next node inside the linked list)
+
+####array = [4,6,8,3], print(array[2])
+
+### access the nect element = cur.next
+
+          prev, cur = None, head
+
+          while cur:
+                temp_nxt = cur.next
+                cur.next = prev
+                prev = cur
+                cur = temp_nxt
+          return prev
