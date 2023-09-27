5aab0173ee2f902dcb0118d39ba2b1c701144626
SaiSirishaR <38254544+SaiSirishaR@users.noreply.github.com>
2023-09-27 09:26:40 +0200

    Update LeetCode1-TwoSum.py

--- LeetCode1-TwoSum.py
+++ LeetCode1-TwoSum.py
@@ -9,3 +9,17 @@ class Solution:
                d[nums[i]] = i
             else:
                 return [ i, d[diff]] 
+
+
+####if the array is sorted
+
+ #      l, r= 0, 1
+       
+ #      while l<r and r < len(nums):
+ #       if nums[l] + nums[r] == target:
+ #           return [l,r]
+ #       elif nums[l] + nums[r] > target:
+ #           r-=1
+ #       elif nums[l] + nums[r] < target:
+ #           l+=1
+ #      return []
