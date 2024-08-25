8411a70aa8dd30992a7bf8801f4cde20d521e487
SaiSirishaR <38254544+SaiSirishaR@users.noreply.github.com>
2024-08-25 17:33:30 +0200

    Update LeetCode153-FindMinimuminRotatedSortedArray.py

--- LeetCode153-FindMinimuminRotatedSortedArray.py
+++ LeetCode153-FindMinimuminRotatedSortedArray.py
@@ -1,29 +1,19 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         
-# Case 1: if left most element is less than the right most element
-# Case 2: if the mid elements is less than the previous and next element
-# Case 3: if mid+1 [or r] is > mid then right side is sorted, go to left vice versa
+        l, r = 0,len(nums)-1
+        tgt = float('inf')
 
-        l ,r =0, len(nums)-1
-       
-        while l<=r:
-            # Case 1
 
-            if nums[l] < nums[r]: 
-                return nums[l]
-            mid = (l+r)//2
+        while l<=r:    
+         if nums[l] < nums[r]:
+            tgt = min(tgt, nums[l])
+            break
 
-             # Case 2
-
-            if nums[mid] < nums[mid-1] and nums[mid] < nums[mid+1]:
-               return nums[mid]  
-
-             # Case 3
-
-            if nums[mid] < nums[r]: 
-                r = mid -1
-            else:
-                l = mid+1           
-
-        
+         mid = (l+r)//2
+         tgt = min(tgt, nums[mid])
+         if nums[l] <= nums[mid]:
+            l = mid+1 
+         else:
+            r = mid -1
+        return tgt      
